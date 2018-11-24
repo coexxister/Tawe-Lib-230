ce72346438fa8c33ff0fff1629a804b1ac5f86eb
Noah Lenagan <noah.lenagan2175@gmail.com>
2018-11-24 15:41:31 +0000

    Added an exception class for duplicate resources. Extends IllegalArgumentException.

---
+++ src/ResourceDuplicateException.java
@@ -0,0 +1,16 @@
+/**
+ * This class represents the exception when there are duplicate resources.
+ * @author Noah Lenagan
+ * @version 1.0
+ */
+public class ResourceDuplicateException extends IllegalArgumentException {
+
+    /**
+     * Creates the class.
+     * @param message The exception message.
+     */
+    ResourceDuplicateException(String message) {
+        super(message);
+    }
+
+}
