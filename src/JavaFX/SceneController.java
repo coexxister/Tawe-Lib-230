5a90e79bb9349c5f646bb4293b6bbb8429a7013f
SinDemon <dominic.woodman@gmail.com>
2018-11-28 17:38:13 +0000

    Staff Interface
    Added fxml for StaffInterface, AccountCreatorInterface,
    AccountsPageInterface and ResourceInterface
    
    Added StaffInterfaceController
    
    Added new scenes to SceneController

--- src/JavaFX/SceneController.java
+++ src/JavaFX/SceneController.java
@@ -14,7 +14,10 @@ public class SceneController {
     public static final String MAIN_INTERFACE = "LoginLoginInterface.fxml";
     public static final String HOME_INTERFACE = "HomeInterface.fxml";
     public static final String DASHBOARD_INTERFACE = "UserDashboardInterface.fxml";
-
+    public static final String STAFF_INTERFACE = "StaffInterface.fxml";
+    public static final String RESOURCE_INTERFACE = "ResourceInterface.fxml";
+    public static final String ACCOUNTS_PAGE_INTERFACE = "AccountsPageInterface.fxml";
+    public static final String ACCOUNT_CREATOR_INTERFACE = "AccountsCreatorInterface.fxml";
 
     public void handleButtonAction(ActionEvent event, String fxml) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource(fxml));
---
+++ src/JavaFX/StaffInterfaceController.java
@@ -0,0 +1,28 @@
+package JavaFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class StaffInterfaceController extends SceneController {
+    @FXML
+    protected void handleLogoutButtonAction(ActionEvent event) throws Exception {
+        handleButtonAction(event, SceneController.MAIN_INTERFACE);
+    }
+    
+    @FXML
+    protected void handleResourcesButtonAction(ActionEvent event) throws Exception {
+    	handleButtonAction(event, SceneController.RESOURCE_INTERFACE);
+    }
+    
+    @FXML
+    protected void handleAccountsButtonAction(ActionEvent event) throws Exception {
+    	handleButtonAction(event, SceneController.ACCOUNTS_PAGE_INTERFACE);
+    }
+    
+    @FXML
+    protected void handleAccountCreatorButtonAction(ActionEvent event) throws Exception {
+    	handleButtonAction(event, SceneController.ACCOUNT_CREATOR_INTERFACE);
+    }
+}
