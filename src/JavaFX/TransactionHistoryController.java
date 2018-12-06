c93b3bafb9d9c00c39b86d8c4f7be1b1f3cf1d00
Noah Lenagan <noah.lenagan2175@gmail.com>
2018-12-06 12:05:10 +0000

    Updates to the design.

--- src/JavaFX/TransactionHistoryController.java
+++ src/JavaFX/TransactionHistoryController.java
@@ -3,8 +3,11 @@ package JavaFX;
 import Core.Transaction;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.layout.VBox;
@@ -27,10 +30,15 @@ public class TransactionHistoryController extends SceneController implements Ini
         Transaction[] transactions = getAccountManager().
                     getTransactionHistory(SceneController.USER_ID);
 
-        Text[] transactionText = new Text[transactions.length];
+        Label[] transactionText = new Label[transactions.length];
 
-        for (int iCount = 0; iCount <= transactions.length; iCount++) {
-            transactionText[iCount] = new Text(transactions[iCount].toString());
+        for (int iCount = 0; iCount < transactions.length; iCount++) {
+            transactionText[iCount] = new Label(transactions[iCount].toString());
+            transactionText[iCount].getStylesheets().add("/Resources/CoreStyle.css");
+            transactionText[iCount].getStyleClass().add("ScrollListItem");
+            transactionText[iCount].setMinHeight(200);
+            transactionText[iCount].setPrefWidth(400);
+            transactionText[iCount].setAlignment(Pos.CENTER);
         }
 
         root.getChildren().addAll(transactionText);
