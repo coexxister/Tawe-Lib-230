0a7f062c2e36ea3dcb5084ab6736b0f3b51d8661
Grzegorz <debicki.grzegorz@outlook.com>
2018-12-08 00:12:50 +0000

    Done searching for all resources.

--- src/JavaFX/ComputerSearchMenuController.java
+++ src/JavaFX/ComputerSearchMenuController.java
@@ -14,4 +14,52 @@ import java.util.ArrayList;
  */
 public class ComputerSearchMenuController extends SceneController{
 
+    @FXML
+    private TextField computerTitle;
+
+    @FXML
+    private TextField computerYear;
+
+    @FXML
+    private TextField computerModel;
+
+    @FXML
+    private TextField computerManufacturer;
+
+    @FXML
+    private TextField computerOS;
+
+    public void handleSearchComputerButtonAction(ActionEvent event) throws IOException {
+        getInput();
+        loadSubscene("/View/ComputerList.fxml");
+    }
+
+    public void getInput(){
+        ArrayList<String> column = new ArrayList<>();
+        ArrayList<String> input = new ArrayList<>();
+        if(!computerTitle.getText().isEmpty()){
+            column.add("Title");
+            input.add("'%" + computerTitle.getText() + "%'");
+        }
+        if(!computerYear.getText().isEmpty()){
+            column.add("Author");
+            input.add("'%" + computerYear.getText() + "%'");
+        }
+        if(!computerModel.getText().isEmpty()){
+            column.add("RYear");
+            input.add(computerModel.getText());
+        }
+        if(!computerManufacturer.getText().isEmpty()){
+            column.add("Genre");
+            input.add("'%" + computerManufacturer.getText() + "%'");
+        }
+        if(!computerOS.getText().isEmpty()){
+            column.add("Genre");
+            input.add("'%" + computerOS.getText() + "%'");
+        }
+
+        setSqlQuery(getResourceManager().createQuery(column.toArray(new String[column.size()]),
+                input.toArray(new String[input.size()]), "Computer"));
+        System.out.println(getSqlQuery());
+    }
 }
